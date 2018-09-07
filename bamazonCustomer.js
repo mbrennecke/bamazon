273e0071200accc150e298f9fb6e487361c4e617
Matthew Brennecke <mbrennecke10@gmail.com>
2018-09-06 20:55:25 -0600

    functionally complete

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -15,7 +15,6 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
   if (err) throw err;
-  // run the start function after the connection is made to prompt the user
   queryAllItems();
 });
 
@@ -25,27 +24,55 @@ function queryAllItems() {
       console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price);
     }
     console.log("-----------------------------------");
+	start();
   });
 }
 
 
 // function which prompts the user for what action they should take
 function start() {
-	
   inquirer
-    .prompt({
-      name: "postOrBid",
-      type: "rawlist",
-      message: "Would you like to [POST] an auction or [BID] on an auction?",
-      choices: ["POST", "BID"]
-    })
+    .prompt([{
+      name: "item",
+      type: "input",
+      message: "What is the ID of the product you would like to buy?",
+    },
+	{
+      name: "quantity",
+      type: "input",
+      message: "How many units would you like to buy?",
+    }]
+	)
     .then(function(answer) {
-      // based on their answer, either call the bid or the post functions
-      if (answer.postOrBid.toUpperCase() === "POST") {
-        postAuction();
-      }
-      else {
-        bidAuction();
-      }
+		var quant = answer.quantity;
+		itemCheck(answer.item, quant);
     });
+}
+
+function itemCheck(item, quant) {
+	
+	connection.query("SELECT * FROM products WHERE ?", { item_id: item }, function(err, res) {
+		var inStock = res[0].stock_quantity;
+		if (inStock < quant) {
+			console.log("Insufficient quantity!");
+			start();
+		} else {
+			var newQuant = inStock - quant;
+			var cost = quant * res[0].price;
+			updateQuant(newQuant, item);
+			console.log("Thank you for the order, your total is " + cost);
+			queryAllItems();
+		}
+	  });
+}
+
+function updateQuant(newQuant, item) {
+	var query = connection.query("UPDATE products SET ? WHERE ?",
+			[{
+				stock_quantity: newQuant
+			},{
+				item_id: item
+			}],  function(err, res) {
+					return;
+				});
 }
