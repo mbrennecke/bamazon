d14f09b1608e8cbfe8dd026204d4420f9d0b3c9d
Matthew Brennecke <mbrennecke10@gmail.com>
2018-09-06 19:52:25 -0600

    so far so good

---
+++ bamazonCustomer.js
@@ -0,0 +1,51 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  port: 3306,
+
+  user: "root",
+
+  password: "root",
+  database: "bamazon"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  // run the start function after the connection is made to prompt the user
+  queryAllItems();
+});
+
+function queryAllItems() {
+  connection.query("SELECT * FROM products", function(err, res) {
+    for (var i = 0; i < res.length; i++) {
+      console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price);
+    }
+    console.log("-----------------------------------");
+  });
+}
+
+
+// function which prompts the user for what action they should take
+function start() {
+	
+  inquirer
+    .prompt({
+      name: "postOrBid",
+      type: "rawlist",
+      message: "Would you like to [POST] an auction or [BID] on an auction?",
+      choices: ["POST", "BID"]
+    })
+    .then(function(answer) {
+      // based on their answer, either call the bid or the post functions
+      if (answer.postOrBid.toUpperCase() === "POST") {
+        postAuction();
+      }
+      else {
+        bidAuction();
+      }
+    });
+}
---
+++ seed.sql
@@ -0,0 +1,14 @@
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Kershaw Chive", "EDC", 35.94, 2),
+("Pocket Pal Sharpener", "EDC", 8.95, 4),
+("Lego Power Functions Motor", "Toys", 22.99, 1),
+("Lego Power Functions Battery", "Toys", 23.95, 3),
+("Parker Jotter", "Office", 12.43, 10),
+("Dreamcity Water Shoes", "Fashion", 23.99, 4),
+("RX Bar Chocolate", "Grocery", 20.67, 19),
+("NES Mini", "Electronics", 59.99, 3),
+("Clip on tuner", "Electronics", 8.79, 12),
+("Basics Lightning USB", "Electronics", 7.99, 50),
+("Hawaiian Shirt, blue flower", "Fashion", 19.98, 8);
+
+SELECT * FROM products;
